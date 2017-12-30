0fb83959a298433930806d0aecec0ccc1d1081b6
patil021 <doddappagouda.malipatil@wipro.com>
2017-12-30 13:14:13 +0530

    added java simple code

---
+++ Simple.java
@@ -0,0 +1,13 @@
+class Simple{
+	public static void main(String args[]){
+		
+			
+	
+		int a=10;
+		float b=a;
+		System.out.println(a);
+		System.out.println(b);
+        00:04 19-12-2017
+		
+			}
+}
